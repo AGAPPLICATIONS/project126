f007478ed70d09350e93dbaeda905b24bd5e9b96
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2023-07-15 09:23:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+lwrx=0;
+rwrx= 0;
+lwry=0;
+rwry=0;
+
 top="";
 inception="";
 peterpantheme="";
@@ -15,7 +20,22 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet= ml5.poseNet(video, modelloaded);
+    posenet.on('pose', gotposes);
 }
 function draw(){
     image(video,0,0,550,450);
+}
+function modelloaded(){
+    console.log("Model is loaded");
+}
+function gotposes(results){
+if(results.length > 0){
+    console.log(results);
+    lwrx=results[0].pose.leftWrist.x;
+    rwrx= results[0].pose.rightWrist.x;
+    lwry= results[0].pose.leftWrist.y;
+    rwry= results[0].pose.rightWrist.y;
+    console.log(lwrx,lwry, rwrx, rwry);
+}
 }

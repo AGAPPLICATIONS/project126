d2d5d8bb9a3c04b0f592ed6d953421d0a59b48ac
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2023-07-29 09:56:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ lwrx=0;
 rwrx= 0;
 lwry=0;
 rwry=0;
-
+lwscore=0;
+songstatus="";
 top="";
 inception="";
 peterpantheme="";
 harrypotterremix="";
 discomusiclol="";
+songfinal="";
+songfinalname="";
 function preload(){
 top = loadSound('TOP2.wav');
 inception= loadSound('inception2.mp3');
-peterpantheme = loadSound('music2.mp3');
+songfinal = loadSound('music2.mp3');
 harrypotterremix = loadSound('music.mp3');
 discomusiclol = loadSound('music3.mp3');
+
 }
 function setup(){
     canvas= createCanvas(550,450);
@@ -25,6 +29,37 @@ function setup(){
 }
 function draw(){
     image(video,0,0,550,450);
+    fill("red");
+    stroke("orange");
+   songstatus= songfinal.isPlaying();
+   if (lwscore> 0.2) {
+    circle(lwrx,lwry,20);
+    inception.stop();
+    songfinal.stop();
+    harrypotterremix.stop();
+    discomusiclol.stop();
+    top.stop();
+ 
+  
+    if (songstatus == false) {
+        if(lwry> 0 && lwry <= 225){
+            songfinal= discomusiclol;
+            songfinalname="Discomusic";
+            songstatus=songfinal.isPlaying();
+        }
+        else if(lwry > 225 && lwry <= 450){
+            songfinal=inception;
+            songfinalname="Inception:time remix";
+            songstatus=songfinal.isPlaying();
+        }
+        songfinal.play();
+        document.getElementById("songplaying").innerHTML= "Now Playing-"+songfinalname;
+
+    }
+
+
+    
+   }
 }
 function modelloaded(){
     console.log("Model is loaded");
@@ -37,5 +72,7 @@ if(results.length > 0){
     lwry= results[0].pose.leftWrist.y;
     rwry= results[0].pose.rightWrist.y;
     console.log(lwrx,lwry, rwrx, rwry);
+    lwscore= results[0].pose.keypoints[9].score;
+    console.log(lwscore);
 }
 }
